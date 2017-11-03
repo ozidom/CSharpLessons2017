ba25513b18c2ff96e2a963b07df56bec496913ed
dom millar <dommillar@gmail.com>
2017-11-03 20:32:17 +1100

    fixed and formats and also added the reference for how a sort works

--- OOIntro/Program.cs
+++ OOIntro/Program.cs
@@ -6,6 +6,13 @@ namespace OOIntro
 {
     class Program
     {
+        //Little helper method to print a list if we need
+        static void PrintList(List<int> items, string title = "Another List")
+        {
+            Console.WriteLine(title);
+            items.ForEach(i => Console.WriteLine(i));
+        }
+
         static void Main(string[] args)
         {
             //Linq basics is looking at a collection of numbers
@@ -16,21 +23,22 @@ namespace OOIntro
             numbers.Sort();
             PrintList(numbers, "Sorted");
 
+			/* If the partition size is fewer than 16 elements, it uses an insertion sort algorithm.
+            If the number of partitions exceeds 2 * LogN, where N is the range of the input array,
+            it uses a Heapsort algorithm. Otherwise, it uses a Quicksort algorithm.
+            Source : MSDN */
+
             //lets find the max number
             int max = numbers.Max();
             Console.WriteLine($"Max {max}");
 
-			//Now lets get the number of items in the list
-			int sum = numbers.Sum();
-			Console.WriteLine($"Sum {sum}");
+            //Now lets get the number of items in the list
+            int count = numbers.Count;
+			Console.WriteLine($"Count {count}");
 
 			//Now lets get the average of items in the list
 			double avg = numbers.Average();
-			Console.WriteLine($"Sum {avg}");
-
-            //Now lets get the number of items in the list
-            int count = numbers.Count();
-            Console.WriteLine($"Count {count}");
+			Console.WriteLine($"Average {avg}");
 
             //Lets add another list and see if we can do an distinct
             var distinct = numbers.Distinct();
@@ -45,18 +53,12 @@ namespace OOIntro
             var intersect = moreNumbers.Intersect(numbers);
             PrintList(intersect.ToList(), "Intersect");
 
-
-            //Home work
-            //TBA
-
+            //Home work 
+            //1. Checkout the following and try to code up some examples:
 
             Console.ReadLine();
         }
 
-        static void PrintList(List<int> items, string title = "Another List")
-        {
-            Console.WriteLine(title);
-            items.ForEach(i => Console.WriteLine(i));
-        }
+       
     }
 }
