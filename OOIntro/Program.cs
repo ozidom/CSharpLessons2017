836101a142639f0ea510a1a7c08a6b6c7ba8eadd
dom millar <dommillar@gmail.com>
2017-11-10 07:17:47 +1100

    Cleaned up and added refs

--- OOIntro/Program.cs
+++ OOIntro/Program.cs
@@ -10,7 +10,7 @@ namespace CSharpLessons
 		{
             Console.WriteLine(listName);
             Console.WriteLine("-------------------------");
-			playerCharacters.ForEach(pc => Console.WriteLine($"{pc.Name} {pc.PlayersRace} {pc.PlayersClass} "));
+            playerCharacters.ForEach(pc => Console.WriteLine($"{pc.Name} {pc.PlayersRace} {pc.PlayersClass} Age {pc.Age} "));
             Console.WriteLine();
 		}
 
@@ -18,14 +18,12 @@ namespace CSharpLessons
         {
             Console.WriteLine("Linq - More");
 
-
-
             var playerCharacters = PlayerCharactedFactory.CreatePersons();
 
             PrintPlayerCharacters(playerCharacters);
 
             var playerCharactersOver200Years = playerCharacters.Where(pc => pc.Age > 200);
-            PrintPlayerCharacters(playerCharactersOver200Years.ToList(),"PC's over 200");
+            PrintPlayerCharacters(playerCharactersOver200Years.ToList(), "PC's over 200");
 
 
 
@@ -52,7 +50,7 @@ namespace CSharpLessons
             //Now lets say we want the PC over 200 who elves
 
             var playerCharactersOver200YearsElfs = playerCharacters.Where(pc => pc.Age > 200).Where(pc => pc.PlayersRace == Race.Elf);
-			PrintPlayerCharacters(playerCharactersOver200YearsElfs.ToList(), "PC's over 200 who are Elves");
+            PrintPlayerCharacters(playerCharactersOver200YearsElfs.ToList(), "PC's over 200 who are Elves");
 
 
 
@@ -91,7 +89,7 @@ namespace CSharpLessons
 
             //Now we just want the number of Elves
             var numberElves = playerCharacters.Where(pc => pc.PlayersRace == Race.Elf).Count();
-			Console.WriteLine($"The number of elves is {numberElves}");
+            Console.WriteLine($"The number of elves is {numberElves}");
 
 
 
@@ -106,13 +104,51 @@ namespace CSharpLessons
 
 
 
-			// Now we just want four oldest
-            playerCharacters.Sort();
-
-			PrintPlayerCharacters(playerCharacters.ToList(), "PC's oldest four");
-
+            // Now we just want four youngest
+            var fourYoungestFailed = playerCharacters.OrderBy(pc => pc.Age).Take(4);
+            PrintPlayerCharacters(fourYoungestFailed.ToList(), "PC's youngest four");
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Deferred Execution - lets find the characters 
+            var charactersVeryHardToHit = playerCharacters.Where(pc => pc.ArmourClass < 5);
+			//can someone tell me what is the state of charactersVeryHardToHit on this line
+			if (charactersVeryHardToHit.Count() > 1)
+            {
+                Console.WriteLine("We have some hard to hit characters");
+            }
+
+
+
+
+
+
+
+
+
+
+
+            //More Linq 
+            // http://linqsamples.com/
+
+            // Homework
+            //http://markheath.net/post/lunchtime-linq-challenge-2
+
+            //More Chaining
+            //https://www.red-gate.com/simple-talk/dotnet/net-framework/linq-secrets-revealed-chaining-and-debugging/
 
 		}
 
