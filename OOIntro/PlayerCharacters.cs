65fbf2f8b54649507e1158a84b5839bfb5ba4b35
dom millar <dommillar@gmail.com>
2017-11-09 23:41:50 +1100

    Initial CommiT

---
+++ OOIntro/PlayerCharacters.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpLessons
+{
+    public enum PlayerCharacterClass
+    {
+        Fighter,
+        Ranger,
+        Cleric,
+        MagicUser,
+        Thief,
+        Paladin
+    }
+
+    public enum Race
+    {
+        Human,
+        Elf,
+        Dwarf,
+        Gnome,
+        Halfling,
+        HalfElf
+	}
+
+	public class PlayerCharacter
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public PlayerCharacterClass PlayersClass { get; set; }
+        public Race PlayersRace{ get; set; }
+        public int HitPoints { get; set; }
+        public int ArmourClass { get; set; }
+
+        public PlayerCharacter()
+        {
+        }
+
+
+    }
+
+    public static class PlayerCharactedFactory
+    {
+        public static List<PlayerCharacter> CreatePersons()
+        {
+            return new List<PlayerCharacter>()
+            { 
+                new PlayerCharacter(){ID=1, Name="John", Age=32, PlayersRace=Race.Human,PlayersClass=PlayerCharacterClass.Paladin,HitPoints=12,ArmourClass=2},
+                new PlayerCharacter(){ID=10, Name="Elfy", Age=321, PlayersRace=Race.Elf,PlayersClass=PlayerCharacterClass.Thief,HitPoints=12,ArmourClass=5},
+                new PlayerCharacter(){ID=101, Name="Steve the Mage", Age=82, PlayersRace=Race.Human,PlayersClass=PlayerCharacterClass.MagicUser,HitPoints=4,ArmourClass=9},
+                new PlayerCharacter(){ID=102, Name="Ranger Joe", Age=492, PlayersRace=Race.Elf,PlayersClass=PlayerCharacterClass.Ranger,HitPoints=8,ArmourClass=4},
+                new PlayerCharacter(){ID=103, Name="Louise the Elf", Age=132, PlayersRace=Race.Human,PlayersClass=PlayerCharacterClass.Cleric,HitPoints=7,ArmourClass=7},
+                new PlayerCharacter(){ID=108, Name="Dwarfo", Age=87, PlayersRace=Race.Dwarf,PlayersClass=PlayerCharacterClass.Fighter,HitPoints=10,ArmourClass=2},
+                new PlayerCharacter(){ID=109, Name="Clyde Foot II", Age=102, PlayersRace=Race.Gnome,PlayersClass=PlayerCharacterClass.Thief,HitPoints=4,ArmourClass=5},
+                new PlayerCharacter(){ID=121, Name="Shorty", Age=67, PlayersRace=Race.Halfling,PlayersClass=PlayerCharacterClass.Thief,HitPoints=7,ArmourClass=6},
+				 new PlayerCharacter(){ID=892, Name="OLD ELF", Age=3432, PlayersRace=Race.Human,PlayersClass=PlayerCharacterClass.Paladin,HitPoints=12,ArmourClass=2},
+				new PlayerCharacter(){ID=999, Name="YOUNG ELF", Age=89, PlayersRace=Race.Elf,PlayersClass=PlayerCharacterClass.Cleric,HitPoints=12,ArmourClass=5},
+				new PlayerCharacter(){ID=1001, Name="GANDALF", Age=2092, PlayersRace=Race.Elf,PlayersClass=PlayerCharacterClass.MagicUser,HitPoints=84,ArmourClass=4},
+				new PlayerCharacter(){ID=1002, Name="Aragorn", Age=40, PlayersRace=Race.Human,PlayersClass=PlayerCharacterClass.Ranger,HitPoints=68,ArmourClass=3},
+				new PlayerCharacter(){ID=1003, Name="Bilbo", Age=67, PlayersRace=Race.Halfling,PlayersClass=PlayerCharacterClass.Thief,HitPoints=6,ArmourClass=4},
+				new PlayerCharacter(){ID=1004, Name="Iyma M", Age=82, PlayersRace=Race.HalfElf,PlayersClass=PlayerCharacterClass.Fighter,HitPoints=10,ArmourClass=2},
+				new PlayerCharacter(){ID=1005, Name="Clyde Foot", Age=102, PlayersRace=Race.Gnome,PlayersClass=PlayerCharacterClass.Thief,HitPoints=4,ArmourClass=5},
+				new PlayerCharacter(){ID=1005, Name="Shorty II", Age=47, PlayersRace=Race.Halfling,PlayersClass=PlayerCharacterClass.Fighter,HitPoints=17,ArmourClass=3}
+            };
+        }
+    }
+
+}
--- OOIntro/Program.cs
+++ OOIntro/Program.cs
@@ -1,113 +1,121 @@
-﻿﻿﻿﻿using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace OOIntro
+namespace CSharpLessons
 {
     class Program
     {
+		static void PrintPlayerCharacters(List<PlayerCharacter> playerCharacters, string listName = "Player Character List")
+		{
+            Console.WriteLine(listName);
+            Console.WriteLine("-------------------------");
+			playerCharacters.ForEach(pc => Console.WriteLine($"{pc.Name} {pc.PlayersRace} {pc.PlayersClass} "));
+            Console.WriteLine();
+		}
+
         static void Main(string[] args)
         {
-            const int basePayRate = 50;
-            Console.WriteLine("Pick the type of Worker: 1.Employee 2.Manager and 3.Junior");
-            int employeeTypeID = int.Parse(Console.ReadLine());
-            BaseEmployee employee;
-            switch (employeeTypeID)
-            {
-                case 1:
-                    employee = new Employee();
-                    break;
-				case 2:
-					employee = new Manager();
-					break;
-				case 3:
-					employee = new Junior();
-					break;
-                default:
-                    employee = null;
-                    break;
-            }
+            Console.WriteLine("Linq - More");
 
-            if (employee is null)
-                throw new Exception("Invalid Employee Type");
-                
 
-			Console.WriteLine("Enter the Name");
-            string name =  Console.ReadLine();
-            employee.Name = name;
 
-			Console.WriteLine("Enter the Hours Worked");
-			int hoursWorked = int.Parse(Console.ReadLine());
+            var playerCharacters = PlayerCharactedFactory.CreatePersons();
 
-            employee.PayRate = basePayRate;
+            PrintPlayerCharacters(playerCharacters);
 
-            int salary = employee.CalculatePay(hoursWorked);
+            var playerCharactersOver200Years = playerCharacters.Where(pc => pc.Age > 200);
+            PrintPlayerCharacters(playerCharactersOver200Years.ToList(),"PC's over 200");
 
-            Console.WriteLine($"{employee.Name} has earned ${salary} this fortnight");
 
-        }
 
-		//Polymorphism
-		/*  The word ‘polymorphism’ literally means ‘a state of having many shapes’ or ‘the capacity to take on different forms’. 
-		 * When applied to object oriented programming languages like Java, it describes a language’s ability to process objects
-		 * of various types and classes through a single, uniform interface. 
-        https://www.sitepoint.com/quick-guide-to-polymorphism-in-java/ */
 
-		//We want to talk about a keyword called "Abstract". To mark a class abstract mean you cannot instantiate it. It's pretty cool though becasue
-        //you can have methods and properties that can be used by sub-classes of an abstract class 
-		//https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/abstract
 
-		// Task 8 We want to support a new type of employee - a junior who gets paid 0.8 of a salary. Lets add another class that inherits from employee
 
-        // Task 9 So lets add some code to ask the user to pick the type of employee and their Name and based on that selection calculate the salary
-        // In this task lets assume the company has a base pay of $50 an hour. We wont worry about DOB and Age in this example but we can leave the implementation
-        // for a later date.
 
-		abstract class BaseEmployee
-        {
-            public string Name { get; set; }
-            public int Age
-            {
-                get
-                {
-                    //https://stackoverflow.com/questions/3152977/calculate-the-difference-between-two-dates-and-get-the-value-in-years
-                    DateTime now = DateTime.Today;
-                    int age = now.Year - DOB.Year;
-                    if (DOB > now.AddYears(-age)) age--;
 
-                    return age;
 
-                }
-            }
-            public DateTime DOB { get; set; }
-            public int PayRate { get; set; }
 
-            public virtual int CalculatePay(int hoursWorked)
-            {
-                return PayRate * hoursWorked;
-            }
-        }
 
-        class Manager : BaseEmployee
-        {
-            public override int CalculatePay(int hoursWorked)
-            {
-                return base.CalculatePay(hoursWorked) * 2;
-            }
-        }
 
-        class Junior : BaseEmployee
-        {
-			public override int CalculatePay(int hoursWorked)
-			{
-                //https://stackoverflow.com/questions/501090/how-do-i-convert-a-decimal-to-an-int-in-c
-				return Convert.ToInt32(base.CalculatePay(hoursWorked) * 0.8);
-			}
-        }
 
-		class Employee : BaseEmployee
-		{
-			
+
+
+
+
+
+
+
+
+
+
+            //Now lets say we want the PC over 200 who elves
+
+            var playerCharactersOver200YearsElfs = playerCharacters.Where(pc => pc.Age > 200).Where(pc => pc.PlayersRace == Race.Elf);
+			PrintPlayerCharacters(playerCharactersOver200YearsElfs.ToList(), "PC's over 200 who are Elves");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Now say we need to have the age of the oldest PC's Who are Dwarf or HalfElf 
+            var oldestDwarfOrHalfElf = playerCharacters.Where(pc => pc.PlayersRace == Race.Dwarf || pc.PlayersRace == Race.HalfElf).Max(pec => pec.Age);
+            Console.WriteLine($"The oldest Dwarf/Half Elf is {oldestDwarfOrHalfElf}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Now we just want the number of Elves
+            var numberElves = playerCharacters.Where(pc => pc.PlayersRace == Race.Elf).Count();
+			Console.WriteLine($"The number of elves is {numberElves}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			// Now we just want four oldest
+            playerCharacters.Sort();
+
+			PrintPlayerCharacters(playerCharacters.ToList(), "PC's oldest four");
+
+
+
+
 		}
-    
-    
-	}
+
+
+    }
 }
